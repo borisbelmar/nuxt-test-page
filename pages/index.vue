77737038ea145bb7942dd35110a3fe707c962592
Boris Belmar <borisbelmarm@gmail.com>
2019-07-24 02:09:42 -0400

    Trabajando en los componentes con bulma

---
+++ pages/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <main>
+    <section class="hero is-primary is-medium">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Bienvenido al Sitio NuxtJS de prueba</h1>
+          <h2 class="subtitle">Un pequeño sitio para hacer pruebas</h2>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <h1 class="title">Artículos del blog</h1>
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column">
+            <div class="box">
+              <div class="content">
+                <h3>Blog de prueba</h3>
+                <p>Contenido de prueba</p>
+                <button class="button is-fullwidth is-primary">Ver más</button>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="card">
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="content">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                  <a href="#">#css</a> <a href="#">#responsive</a>
+                  <br>
+                  <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
