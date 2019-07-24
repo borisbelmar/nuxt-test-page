5ebb0c626abb1ac8df959c04ba41d60e21b31ba5
Boris Belmar <borisbelmarm@gmail.com>
2019-07-24 18:34:18 -0400

    Avanzando un poco

---
+++ components/BlogCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="card">
+        <div class="card-image">
+            <figure class="image is-16by9">
+                <img :src="content.img.url" :alt="content.img.alt">
+            </figure>
+        </div>
+        <div class="card-content">
+            <div class="content">
+                <h3 class="title">{{content.title}}</h3>
+                <p>{{content.content}}</p>
+                <div class="tags">
+                    <a v-for="(tag, index) of content.tags" :key="index" class="tag is-primary" href="#">{{tag}}</a>
+                </div>
+                <p>
+                    <small>{{content.date}}</small>
+                </p>
+                <a :href="content.link">
+                    <button class="button is-primary is-fullwidth">Ir al artículo</button>
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BlogCard',
+    props: {
+        content: Object
+    }
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -11,37 +11,24 @@
     <section class="section">
       <div class="container">
         <div class="columns">
+          <div class="column has-text-centered">
+            <div class="content is-large">
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis earum blanditiis pariatur. Alias architecto hic veniam saepe praesentium neque fugit quasi natus at? Officiis aspernatur, dolores doloribus excepturi sint deleniti.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="container">
+        <div class="columns">
           <div class="column">
             <h1 class="title">Artículos del blog</h1>
           </div>
         </div>
         <div class="columns">
-          <div class="column">
-            <div class="box">
-              <div class="content">
-                <h3>Blog de prueba</h3>
-                <p>Contenido de prueba</p>
-                <button class="button is-fullwidth is-primary">Ver más</button>
-              </div>
-            </div>
-          </div>
-          <div class="column">
-            <div class="card">
-              <div class="card-image">
-                <figure class="image is-4by3">
-                  <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-                </figure>
-              </div>
-              <div class="card-content">
-                <div class="content">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                  <a href="#">#css</a> <a href="#">#responsive</a>
-                  <br>
-                  <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-                </div>
-              </div>
-            </div>
+          <div class="column" v-for="(item, index) of articles" :key="index">
+            <BlogCard :content="item" />
           </div>
         </div>
       </div>
@@ -50,8 +37,50 @@
 </template>
 
 <script>
+import BlogCard from '@/components/BlogCard.vue';
 export default {
-  
+  components: {
+    BlogCard
+  },
+  data() {
+    return {
+      articles: [
+        { 
+          title: 'Artículo 1',
+          img: {
+            url: 'https://bulma.io/images/placeholders/1280x960.png',
+            alt: 'Foto artículo 1'
+          },
+          tags: ['tag1', 'tag2'], 
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
+          date: '11:09 PM - 1 Jan 2016',
+          link: 'articulo-1'
+        },
+        { 
+          title: 'Artículo 2',
+          img: {
+            url: 'https://bulma.io/images/placeholders/1280x960.png',
+            alt: 'Foto artículo 2'
+          },
+          tags: ['tag1', 'tag2', 'tag3'], 
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
+          date: '11:09 PM - 1 Jan 2016',
+          link: 'articulo-2'
+        },
+        { 
+          title: 'Artículo 3',
+          img: {
+            url: 'https://bulma.io/images/placeholders/1280x960.png',
+            alt: 'Foto artículo 3'
+          },
+          tags: ['tag1'], 
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
+          date: '11:09 PM - 1 Jan 2016',
+          link: 'articulo-3'
+        }
+      ]
+    }
+  }
 }
 </script>
 
